711306d1a6959fe57e5dbf6a5560da5727dd220e
YashuQA <yaseswiniy@duck.com>
2023-07-21 13:14:21 +0530

    Switch Case Statement

---
+++ src/FlowControlStatements/SwitchStatement.java
@@ -0,0 +1,27 @@
+package FlowControlStatements;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+		
+		int age = 5;
+		
+		switch(age) {
+		
+		case 1: 
+			System.out.println("Case One");
+			break;
+		case 2:
+			System.out.println("Case Two");
+			break;
+		case 3:
+			System.out.println("Case Three");
+			break;
+		default:
+			System.out.println("default case!");
+				break;
+		}
+
+	}
+
+}
